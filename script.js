096e5914045645bc52300848b9faffc117c49e7d
ndirangu brian <ndirangubrian388@gmail.com>
2025-11-17 00:50:38 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const name="brian";
+console.log(name);
+const study="i am learning js";
+console.log(study);  
+const calculate_sum = 10 + 10 + 10;
+console.log(calculate_sum);
+const calculate_difference =  calculate_sum -15;
+console.log(calculate_difference);
